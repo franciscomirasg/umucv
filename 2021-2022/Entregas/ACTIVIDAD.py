0892de4beaafdf2931443b02a8dd5edb2b33b4c7
Francisco Miras Garcia <francisco.mirasg@gmail.com>
2022-03-06 17:03:00 +0100

    Comment Activity

--- 2021-2022/Entregas/ACTIVIDAD.py
+++ 2021-2022/Entregas/ACTIVIDAD.py
@@ -26,11 +26,16 @@ from collections import deque
 # ---------------------------------------------------------------------------
 
 TIME_OF_RECORDING = 2  # Tiempo de guardado en segundos
-FPS = 30
-TOTAL_FRAMES_SAVE = TIME_OF_RECORDING * FPS
-SUAVIZAR_MEDIA = 10
-UMBRAL_INICIAL_DETEC = 10
-UMBRAL_INICIAL_RECORTE = 100
+FPS = 30  # FPS de la camara
+TOTAL_FRAMES_SAVE = TIME_OF_RECORDING * FPS  # Tamaño de frames para guardar el tiempo requerido
+SUAVIZAR_MEDIA = 10  # Numero de medias como referecia
+'''
+Se tiene en cuenta el valor medio de las ultimas medias para suavizar las diferencias en situacion de iluminación
+variable, natural o con una camara con flickering o autofocus
+'''
+
+UMBRAL_INICIAL_DETEC = 10  # Valor inicial del umbral de detección
+UMBRAL_INICIAL_RECORTE = 100  # Valor inicial del umbral de recorte
 
 cv.namedWindow('input')
 
@@ -39,12 +44,12 @@ cv.namedWindow('input')
 # Class
 # ---------------------------------------------------------------------------
 
-class Estado(enum.Enum):
+class Estado(enum.Enum):  # Enumerado con los estados
     ACTIVITY = 1
     END = 2
 
 
-class ControlAct:
+class ControlAct:  # Clase contenedora con las variables usadas durante la ejecucion
     region: ROI
     saved_trozo: np.ndarray
     last_frames: deque
@@ -69,6 +74,24 @@ class ControlAct:
         self.last_frames.clear()
         self.last_mean.clear()
 
+    def start_video(self):  # Inicia el video y guarda los ultimos x segundos de inmediato
+        if self.video or len(self.last_frames) < 1:
+            return
+        self.video = Video(fps=FPS, codec="MJPG", ext="avi")
+        self.video.ON = True
+        for f in self.last_frames:
+            self.video.write(f)
+        self.last_frames.clear()
+
+    def continue_video(self, f):  # Añade el frame actual
+        if self.video:
+            self.video.write(f)
+
+    def stop_video(self):  # Detiene el video
+        self.video.ON = False
+        self.video.release()
+        self.video = None
+
 
 data = ControlAct(ROI('input'))
 
@@ -76,6 +99,7 @@ data = ControlAct(ROI('input'))
 # ---------------------------------------------------------------------------
 # Functions
 # ---------------------------------------------------------------------------
+
 
 def update_umbral(v):
     v = max(1, v)
@@ -98,27 +122,6 @@ def gray2bgr(x):
     return r
 
 
-def start_video(data: ControlAct):
-    if data.video or len(data.last_frames) < 1:
-        return
-    data.video = Video(fps=FPS, codec="MJPG", ext="avi")
-    data.video.ON = True
-    for f in data.last_frames:
-        data.video.write(f)
-    data.last_frames.clear()
-
-
-def continue_video(data: ControlAct, f):
-    if data.video:
-        data.video.write(f)
-
-
-def stop_video(data: ControlAct):
-    data.video.ON = False
-    data.video.release()
-    data.video = None
-
-
 # ---------------------------------------------------------------------------
 # INIT
 # ---------------------------------------------------------------------------
@@ -131,34 +134,34 @@ cv.createTrackbar('Umbral Recorte', 'input', UMBRAL_INICIAL_RECORTE, 1000, updat
 # ---------------------------------------------------------------------------
 
 for key, frame in autoStream():
-    if data.region.roi:
+    if data.region.roi:  # Si se ha seleccionado una región, la recortamos
         [x1, y1, x2, y2] = data.region.roi
         recorte = frame[y1:y2, x1:x2]
 
-        if key == ord('x'):
+        if key == ord('x'):  # Pulsar x para eleminar región
             data.region.roi = None
             data.reset_trozo()
 
-        if key == ord('c'):
+        if key == ord('c'):  # Pulsar c para capturar la región
             if data.saved_trozo is not None:
                 data.reset_trozo()
             data.saved_trozo = recorte
             cv.imshow('Trozo Seleccionado', data.saved_trozo)
 
-        if data.saved_trozo is not None:
-            diff = cv.absdiff(data.saved_trozo, recorte)
+        if data.saved_trozo is not None:  # Si hay trozo guardado, empezamos con la vigilancia
+            diff = cv.absdiff(data.saved_trozo, recorte)  # Calculamos la diferencia del actual con el guardado
             diff = bgr2gray(diff)
             mean = np.mean(diff)
-            means = mean
+            means = mean  # La media nos da una aproximación de cuanto ha cambiado con la de referncia
 
-            if len(data.last_mean) >= SUAVIZAR_MEDIA:
+            if len(data.last_mean) >= SUAVIZAR_MEDIA:  # Si hay suficientes medias las suavizamos conforme el tiempo
                 means = np.mean(data.last_mean)
 
-            if np.abs(means - mean) <= data.umbral_deteccion:
+            if np.abs(means - mean) <= data.umbral_deteccion:  # Si la variación no es suficiente grande.
                 data.last_mean.append(mean)
-                data.last_frames.append(recorte)
-                if data.estado is not Estado.END:
-                    stop_video(data)
+                data.last_frames.append(recorte)    # Actualizamos las medias y guardamos frame
+                if data.estado is not Estado.END:   # Si venimos de un estado de actividad
+                    data.stop_video()               # Detenemos grabación y destruimos ventanas residuales
                     print('Fin actividad')
                     data.estado = Estado.END
                     try:
@@ -166,21 +169,22 @@ for key, frame in autoStream():
                         cv.destroyWindow('objeto')
                     except Exception:
                         pass
-            else:
-                mask = diff > data.umbral_recorte
+            else:   # Si Hay diferencia
+                mask = diff > data.umbral_recorte # Creamos mascara
                 cv.imshow('mascara', mask.astype(float))
                 mask = gray2bgr(mask)
-                objeto = np.zeros_like(recorte)
+                objeto = np.zeros_like(recorte) # Recortamos la mascara
                 np.copyto(objeto, recorte, where=mask == 1)
                 cv.imshow('objeto', objeto)
-                putText(diff, 'ALERT', orig=(5, diff.shape[0] - 5))
+                putText(diff, 'ALERT', orig=(5, diff.shape[0] - 5)) # Notificamos la alerta
 
-                if data.estado is Estado.END:
+                if data.estado is Estado.END: # Si no estamos grabando, empezamos
                     print('Actividad detectada')
-                    start_video(data)
+                    data.start_video()
+                    data.continue_video(recorte)
                     data.estado = Estado.ACTIVITY
-                elif data.estado is Estado.ACTIVITY:
-                    continue_video(data, recorte)
+                elif data.estado is Estado.ACTIVITY:    # Continuamos grabando
+                    data.continue_video(recorte)
 
             putText(diff, f'Mean(t) = {np.round(means, 4)}', orig=(5, 16 * 2))
             putText(diff, f'Mean = {np.round(mean, 4)}', orig=(5, 16))
