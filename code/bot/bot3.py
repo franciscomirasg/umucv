4a6f993ec4c8726ae879504782287bacfb9ed3ee
Francisco Miras Garcia <francisco.mirasg@gmail.com>
2022-03-16 13:36:01 +0100

    Add .env support

--- code/bot/bot3.py
+++ code/bot/bot3.py
@@ -3,6 +3,7 @@
 # comando con argumentos
 # y procesamiento de una imagen
 # enviada por el usuario
+import os
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from io import BytesIO
@@ -11,48 +12,52 @@ import cv2 as cv
 import skimage.io as io
 import numpy as np
 
-from mybotid import myid, mybot
+from dotenv import load_dotenv
 
-updater = Updater(mybot)
+load_dotenv()
+
+updater = Updater(os.environ['TOKEN'])
 
 Bot = updater.bot
 
+
 def sendImage(userid, frame):
     frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    image = Image.fromarray(frame, mode = 'RGB')
+    image = Image.fromarray(frame, mode='RGB')
     byte_io = BytesIO()
     image.save(byte_io, 'PNG')
     byte_io.seek(0)
     Bot.sendPhoto(chat_id=userid, photo=byte_io)
 
-def hello(update,_):
+
+def hello(update, _):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
+
 
 def argu(update, _):
     print(update)
     args = update.message.text.split()
     print('arguments:')
     for arg in args:
-       print(arg)
+        print(arg)
     update.message.reply_text(str(sum([int(a) for a in args[1:]])))
 
-def work(update,_):
+
+def work(update, _):
     file_id = update.message.photo[-1].file_id
     path = Bot.get_file(file_id)['file_path']
     print(path)
     img = io.imread(path)
     print(img.shape, update.message.from_user.first_name)
-    update.message.reply_text('{}x{}'.format(img.shape[1],img.shape[0]))
+    update.message.reply_text('{}x{}'.format(img.shape[1], img.shape[0]))
     r = cv.cvtColor(cv.cvtColor(img, cv.COLOR_RGB2GRAY), cv.COLOR_GRAY2RGB)
     r = np.fliplr(r)
     sendImage(update.message.chat.id, r)
 
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(CommandHandler('argu' , argu))
+updater.dispatcher.add_handler(CommandHandler('argu', argu))
 updater.dispatcher.add_handler(MessageHandler(Filters.photo, work))
-
 
 updater.start_polling()
 updater.idle()
-
